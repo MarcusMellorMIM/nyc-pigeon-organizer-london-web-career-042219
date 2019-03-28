85f39c53feb3d5eb864e847eca972b8bd84c9f58
Marcus Mellor <marcus@mellor.com>
2019-03-28 13:17:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  new_data={}
+  
+  data.each do |pigeon_key, key_values|
+  
+    key_values do | attr_key, attr_value |
+      
+      attr_value each do | name |
+      if !new_data[name]
+        new_data[name]={}
+      end
+      if !new_data[name][attr_key]
+        new_data[name][attr_key]=
+      end
+      new_data
+        
+      end
+    end
+    
+  end
+
 end
